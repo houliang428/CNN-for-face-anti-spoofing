0a6197f40fce66554557537fc99ec95a5e2b6a6f
houliang428 <503683857@qq.com>
2018-09-26 09:22:12 +0800

    Update videocut.py

--- Video cutframe/videocut.py
+++ Video cutframe/videocut.py
@@ -11,15 +11,15 @@ import app_console
 
 Video_dir = '' 
 store_dir = ''
-#视频保存目录以及截取后图像保存地址
+
 
 real_count = 0
 fake_count = 0
 
-for i in range(1, 13): #每个人12段视频，依次读入每段视频
+for i in range(1, 13): #Each person has 12 videos
     vc = cv2.VideoCapture(Video_dir+str(i)+'.avi') 
-    flag, frame = vc.read()  #flag 是否存在下一帧； frame: 当前视频帧
-#计算视频总帧数
+    flag, frame = vc.read() 
+#Calculating total frames
     frame_count = 0
     while(flag):
         ret, frame = vc.read()
@@ -29,8 +29,8 @@ for i in range(1, 13): #每个人12段视频，依次读入每段视频
     print(frame_count)
     vc.release()
 
-    gap = frame_count//20   #均匀间隔截取20帧
-    c = 1   #当前帧序号计数
+    gap = frame_count//20   
+    c = 1   
     
     vc = cv2.VideoCapture(Video_dir+str(i)+'.avi')
     flag, frame = vc.read()
@@ -44,15 +44,15 @@ for i in range(1, 13): #每个人12段视频，依次读入每段视频
                         target_image = app_console.face_crop(frame,240,240)
                     else:
                         target_image = app_console.face_crop(frame,600,800)
-                        #高清视频文件人像较大，故截取600*800大小的帧
-                    cv2.imwrite(store_dir+'real'+'.' + str(real_count) + '.png',target_image)    #存储为图像
+
+                    cv2.imwrite(store_dir+'real'+'.' + str(real_count) + '.png',target_image)    
                     real_count = real_count + 1
                 else:
                     if(i<=9):
                         target_image = app_console.face_crop(frame,240,240)
                     else:
                         target_image = app_console.face_crop(frame,600,800)
-                    cv2.imwrite(store_dir+'fake'+'.' + str(fake_count) + '.png',target_image)    #存储为图像
+                    cv2.imwrite(store_dir+'fake'+'.' + str(fake_count) + '.png',target_image)   
                     fake_count = fake_count + 1
             c = c + 1
     cv2.waitKey(1)
